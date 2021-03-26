1789aea88a3f2a7379757e63fd7f614040a17090
meglin97 <78850286+meglin97@users.noreply.github.com>
2021-03-26 15:12:42 +0100

    Update main.c

--- code/main.c
+++ code/main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <complex.h>
 
+double fonct_Hjw(double l,double r,double c,double freq){
+    double w=2*M_PI*freq;
+    return r/(r+I*(l*w-(1/c*w)));
+}
+
+int main(){
+    
+}
