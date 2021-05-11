67566ddea0c1a25a77b84b2084273efa66fc7ea5
meglin97 <78850286+meglin97@users.noreply.github.com>
2021-05-11 22:09:19 +0200

    Update plot_system.py

--- plot_system.py
+++ plot_system.py
@@ -14,14 +14,18 @@ plt.plot(euler[:,0],euler[:,1])
 plt.plot(heun[:,0],heun[:,1])
 plt.plot(rk4[:,0],rk4[:,1])
 
+# Add legend, axis label  and title
 plt.legend(["euler method","heun method","RK4 method"],shadow=False, ncol=3)
 plt.title('Modélisation d\'un circuit RLC (E= 1V, R=0.1Ω, L=1H, C=1F)')
 plt.xlabel("temps en s")
 plt.ylabel("u(t)")
 plt.xlim([-1,101])
 plt.grid()
+
+# Save result as image
 plt.savefig('data.png')
 
+# Alert message to user
 print("\n********** INFO : results.png crée **********")
 
 
